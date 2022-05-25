69a9d4760849e0d5e70614450b1bbf98208968f2
proTuna <tunaman629@gmail.com>
2022-05-25 14:04:31 +0900

    Four Squares

---
+++ dp/17626.py
@@ -0,0 +1,16 @@
+from glob import glob
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+dp = [0]*(n+1)
+dp[1] = 1
+for i in range(2,n+1):
+    MIN = int(1e9)
+    j = 1
+    while (j**2) <= i:
+        MIN = min(MIN, dp[i-(j**2)])
+        j+=1
+    dp[i]=MIN+1
+
+print(dp[n])

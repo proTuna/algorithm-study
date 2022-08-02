bbb9fb35521a0df84fb997bc5bac7a06f3215577
proTuna <tunaman629@gmail.com>
2022-08-02 22:34:01 +0900

    N과 M (2)

---
+++ Backtracking/15650.py
@@ -0,0 +1,19 @@
+import sys
+n,m = map(int,input().split())
+
+check = [False]*(n+1)
+a = [0]*m
+
+def go(index,start,n,m):
+    if index ==m:
+        sys.stdout.write(' '.join(map(str,a))+'\n')
+        return
+    for i in range(start,n+1):
+        if check[i]:
+            continue
+        check[i] = True
+        a[index] = i
+        go(index+1,i+1,n,m)
+        check[i]= False
+
+go(0,1,n,m)

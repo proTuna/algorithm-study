9c2d99cd953bbe8329b0c290333cc1d22edf12c2
proTuna <tunaman629@gmail.com>
2022-11-02 18:04:55 +0900

    LCS

---
+++ dp/9251.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+a = input().rstrip()
+b = input().rstrip()
+h,w = len(a),len(b)
+dp = [[0]*(w+1) for _ in range(h+1)]
+
+for i in range(1,h+1):
+    for j in range(1,w+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+
+print(dp[-1][-1])

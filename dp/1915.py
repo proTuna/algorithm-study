9467951620f873c212ce3d328b5bd5ed7d3dc6af
proTuna <tunaman629@gmail.com>
2022-11-03 18:54:57 +0900

    가장 큰 정사각형

---
+++ dp/1915.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().rstrip().split())
+board = [list(map(int,input().rstrip())) for _ in range(n)]
+dp = [[0]*m for _ in range(n)]
+
+answer = 0
+
+for i in range(n):
+    for j in range(m):
+        if i == 0 or j == 0:
+            dp[i][j] = board[i][j]
+        elif board[i][j] == 0:
+            dp[i][j] = 0
+        else:
+            dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+        answer = max(dp[i][j], answer)
+print(answer**2)
